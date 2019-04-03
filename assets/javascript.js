917ae0ee812252e77955c2e6860e3e72ea296cb1
Gonzo <aarongonzo31@gmail.com>
2019-04-02 21:31:53 -0700

    hw commit

--- assets/javascript.js
+++ assets/javascript.js
@@ -11,25 +11,21 @@ firebase.initializeApp(config);
 
 // create a varibale to reference the datatbase
 var database = firebase.database();
+console.log("database: ", database);
 
 // Use empty strings as intial values for the input
-var name = "";
+var trainName = "";
 var destination = "";
 var firstTrain = "";
 var frequency = "";
 
-/* $(document).ready(function() {
-  database.ref().on("value", function(snapshot) {
-    var response = snapshot.val();
-    console.log(response);
-  });
- }); */
-
+// create an on click function for the submit button,
 $("#add-train").on("click", function() {
-  name = $("#train-input")
+  console.log("clicked on add-train");
+  trainName = $("#train-input")
     .val()
     .trim();
-  console.log(name);
+  console.log(trainName);
   destination = $("#destination-input")
     .val()
     .trim();
@@ -40,10 +36,32 @@ $("#add-train").on("click", function() {
     .val()
     .trim();
 
-  firebase.database().set({
-    trainname: name,
+  // this is where we push the information into our database
+  database.ref().push({
+    trainName: trainName,
     destination: destination,
-    firstTrain: firsttraintime,
+    firstTrain: firstTrain,
     frequency: frequency
   });
 });
+
+// this holding the value of the child added
+database.ref().on("child_added", function(childsnapshot) {
+  console.log(childsnapshot.val());
+  var trainName = childsnapshot.val().trainName;
+  var destination = childsnapshot.val().destination;
+  var firstTrain = childsnapshot.val().firstTrain;
+  var frequency = childsnapshot.val().frequency;
+});
+
+// this will change the html display with the new snapshot input value
+database
+  .ref()
+  .orderByChild("dateAdded")
+  .limitToLast(1)
+  .on("child_added", function(snapshot) {
+    $("#train-display").text(snapshot.val().trainName);
+    $("#destination-display").text(snapshot.val().destination);
+    $("#frequency-display").text(snapshot.val().frequency);
+    $("#next-arrival-display").text(snapshot.val().firstTrain);
+  });
